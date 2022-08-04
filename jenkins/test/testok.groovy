986bb5edf6ac14eae5a227470f98a0e7ce91c891
Andrei Poliakov <avpgenium@yandex.ru>
2022-08-04 19:11:35 +0300

    add docker and jenkins

---
+++ jenkins/test/testok.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+
+    stages {
+        stage("Clone sources") {
+            steps {
+                git url: 'https://github.com/tkgregory/sonarqube-jacoco-code-coverage.git'
+            }
+        }
+
+        stage("SonarQube analyses") {
+            steps {
+                withSonarQubeEnv('SonarQube') {
+                    sh "./gradlew sonarqube -Dsonar.login=8cae112e16a890fe03410604aa2a1ef724d1ead3"
+                }
+            }
+        }
+
+        stage("Quality gate") {
+            steps {
+                waitForQualityGate abortPipeline: true
+            }
+        }
+    }
+}
+
+
+
