c5e831b57b9b4e29ece8470fb3761c380670adac
Vasiliy Bolshakov <logikv@yandex.ru>
2023-09-27 00:49:36 +0300

    fix building

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,9 +14,7 @@ pipeline {
 
         stage('Build common libs') {
             steps {
-                git branch: params.BRANCH ,url: 'https://github.com/jenkins-examples-1/common-libs.git'
-
-                sh "git checkout ${params.BRANCH}"
+                git branch: params.BRANCH, url: 'https://github.com/jenkins-examples-1/common-libs.git'
 
                 sh "mvn clean package"
             }
@@ -35,12 +33,9 @@ pipeline {
 
                         stage('build streaming') {
                             steps {
-                                git branch: params.BRANCH ,url: 'https://github.com/jenkins-examples-1/streaming'
-
-                                sh "git checkout ${params.BRANCH}"
+                                git branch: params.BRANCH, url: 'https://github.com/jenkins-examples-1/streaming.git'
 
                                 sh "mvn clean package"
-
                             }
                         }
 
@@ -52,21 +47,19 @@ pipeline {
                                 }
 
                                 sh """cd target
-                                      cp *.jar /opt/services/streaming/
+                                      curl -i -X PUT "http://namenode:9870/webhdfs/v1/streaming_${params.BRANCH}/common-libs-${project_version}.jar?op=CREATE&overwrite=true"
+                                      curl -i -X PUT  --upload-file streaming-1.0-SNAPSHOT.jar "http://datanode:9864/webhdfs/v1/streaming_${params.BRANCH}/common-libs-${project_version}.jar?op=CREATE&namenoderpcaddress=namenode:9000&createflag=&createparent=true&overwrite=true"
                                    """
                             }
                         }
                     }
                 }
-
                 stage('batch') {
                     stages {
 
                         stage('build batching') {
                             steps {
-                                git branch: params.BRANCH ,url: 'https://github.com/jenkins-examples-1/batching'
-
-                                sh "git checkout ${params.BRANCH}"
+                                git branch: params.BRANCH, url:  'https://github.com/jenkins-examples-1/batching.git'
 
                                 sh "mvn clean package"
                             }
@@ -76,13 +69,14 @@ pipeline {
                             steps {
                                 sh """cd target
                                   curl -i -X PUT "http://namenode:9870/webhdfs/v1/batch_${params.BRANCH}/batching-1.0-SNAPSHOT.jar?op=CREATE&overwrite=true"
-                                  curl -i -X PUT -T test_jenkins_mvn-1.0-SNAPSHOT.jar "http://datanode:9864/webhdfs/v1/batch_${params.BRANCH}/batching-1.0-SNAPSHOT.jar?op=CREATE&namenoderpcaddress=namenode:9000&createflag=&createparent=true&overwrite=true"
+                                  curl -i -X PUT --upload-file batching-1.0-SNAPSHOT.jar "http://datanode:9864/webhdfs/v1/batch_${params.BRANCH}/batching-1.0-SNAPSHOT.jar?op=CREATE&namenoderpcaddress=namenode:9000&createflag=&createparent=true&overwrite=true"
                                    """
                             }
                         }
 
                     }
                 }
+              
 
                 stage('rest') {
                     stages {
